47158e0f78d99ab245958fb2c621ccb231c280c9
Kholy <37966550+AhmadAlkholy@users.noreply.github.com>
2018-12-02 18:42:07 +0200

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -50,7 +50,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'cc8cad2012e60b0b69d3',
-    cluster: 'eu',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     encrypted: true
 });
